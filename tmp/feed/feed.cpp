41493c80618904e7dd53be769b4d0f5bb665c484
Tfangz <tfangz888@gmail.com>
2026-06-11 10:36:54 +0800

    Update feed.cpp

--- tmp/feed/feed.cpp
+++ tmp/feed/feed.cpp
@@ -1,4 +1,4 @@
-// g++ -o a -g -O0 a.cpp c.o -I. -lpthread
+// g++ -o a -g -O0 feed.cpp c.o -I. -lpthread
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -21,7 +21,7 @@ int main() {
 
     singleRow= knk(4, ks((S) "ABC"), kf(10.0), kj(20), kf(1000001.0));
     // Perform single row insert, tickerplant will add timestamp column itself
-    //// 异步调用, 句柄前加负号, 不需要 r0 释放result. 同步调用需要释放
+    //////// 异步调用, 句柄前加负号, 不需要 r0 释放result. 同步调用需要释放
     result= k(-handle, ".u.upd", ks((S) "md"), singleRow, (K)0); 
     if(isRemoteErr(result)) {
         kclose(handle);
@@ -43,6 +43,7 @@ int main() {
 } 
 #endif
 
+
 #if 1
 int main() {
     int i, n= 3;
@@ -60,20 +61,20 @@ int main() {
     K multipleRow= knk(4, ktn(KS, n), ktn(KF, n), ktn(KJ, n), ktn(KF, n));
     for(i= 0; i < n; i++) {
         kS(kK(multipleRow)[0])[i]= ss(symbols[i % n]);
-        kF(kK(multipleRow)[1])[i]= 10.0 * i;
-        kJ(kK(multipleRow)[2])[i]= i;
-        kF(kK(multipleRow)[3])[i]= 100000.0 * i;
+        kF(kK(multipleRow)[1])[i]= 100.0 * (i+1);
+        kJ(kK(multipleRow)[2])[i]= i+1;
+        kF(kK(multipleRow)[3])[i]= 100000.0 * (i+1);
     }
 
     // Perform multiple row insert, tickerplant will add timestamp column itself
-    //// 异步调用, 句柄前加负号, 不需要 r0 释放result. 同步调用需要释放
-    result= k(handle, ".u.upd", ks((S) "md"), multipleRow, (K)0);
+    ////// 异步调用, 句柄前加负号, 不需要 r0 释放result. 同步调用需要释放
+    result= k(-handle, ".u.upd", ks((S) "md"), multipleRow, (K)0);
     if(isRemoteErr(result)) {
         kclose(handle);
         return EXIT_FAILURE;
     }
 
-    r0(result);
+    //r0(result);
     kclose(handle);
     return EXIT_SUCCESS;
 }
